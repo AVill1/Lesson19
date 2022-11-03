68217cf4dca79388b02a5ecf4a228f4b6de6e005
Avill1 <anastasiya6379753@gmail.com>
2022-11-03 21:53:24 +0300

    Commit new modul

--- students.py
+++ students.py
@@ -2,14 +2,19 @@ class Student:
     """class description of student"""
 
     def __init__(self, name, age,mark=4, alive=True):
-        self.name = name
-        self.age = age
-        self.mark = mark
-        self.alive = alive
+        self._name = name
+        self._age = age
+        self._mark = mark
+        self._alive = alive
 
     def __str__(self):
-        msg = self.name
-        msg += (" is still alive " if self.alive else " is dead ")
-        msg += "(mark: " + str(self.mark) + ")"
+        msg = self._name
+        msg += (" is still alive " if self._alive else " is dead ")
+        msg += "(mark: " + str(self._mark) + ")"
 
         return msg
+
+    name = property(get_name, set_name)
+    age = property(get_age, set_age)
+    mark = property(get_mark, set_mark)
+    alive = property(get_alive, set_alive)
